c0c43f74fb1cb565bd607688757faad10cee8942
Noah John Ucab <noahjohn.ucab@gmail.com>
2017-02-22 09:16:52 +0800

    config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
     // First application
     {
-      name      : "carcarecapp",
+      name      : "theguardianapp",
       script    : "./server.js",
       env_production : {
         NODE_ENV: "production"
@@ -25,9 +25,9 @@ module.exports = {
       user : "carcareapp",
       host : "138.197.108.171",
       ref  : "origin/master",
-      repo : "https://github.com/noahjohn9259/carcareapp.git",
-      path : "/home/carcareapp/www",
-      "post-deploy" : "nvm install && npm install && npm rebuild node-sass && npm run build-pm2 && ~/.nvm/versions/node/v6.9.4/bin/pm2 startOrRestart ~/www/source/ecosystem.config.js --env production"
+      repo : "https://github.com/noahjohn9259/the-guardian-app.git",
+      path : "/home/carcareapp/guardianapp",
+      "post-deploy" : "nvm install && npm install && npm rebuild node-sass && npm run build-pm2 && ~/.nvm/versions/node/v6.9.4/bin/pm2 startOrRestart ~/guardianapp/source/ecosystem.config.js --env production"
     }
   }
 }
